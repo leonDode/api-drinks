fadc866a8c848eafb82f3c5d927c4e6a2ebbd977
dode2511 <leondodejr@hotmail.com>
2024-06-30 00:42:27 -0300

    basic services and drinks entity

---
+++ src/drinks/drinks.entity.ts
@@ -0,0 +1,17 @@
+export class Drinks {
+    id: Number
+
+    nome: String
+
+
+    descricao: string
+
+
+    tags: string[]
+
+    ingredientes: string[]
+
+
+    salvo: boolean
+}
+
--- src/drinks/drinks.service.ts
+++ src/drinks/drinks.service.ts
@@ -1,6 +1,70 @@
 import { Injectable } from '@nestjs/common';
+import { Drinks } from './drinks.entity';
 
 @Injectable()
 export class DrinksService {
-    
+    private drinks: Drinks[] =[
+        {
+            id: 1,
+            nome: 'mojito',
+            descricao: 'afakonajbcasjc asbcaspibcaps',
+            tags:['doce','refrescante'],
+            ingredientes: ['açucar','hortela','rum'],
+            salvo: true
+
+        },
+    ]
+
+    findAll(){
+        return this.drinks
+    }
+
+
+    findByName(nome:string){
+        return this.drinks.find(drinks => drinks.nome === nome)
+    }
+
+    findOne(id:number){
+        return this.drinks.find(drinks => drinks.id === id)
+    }
+
+
+    create(createDrinkDTO:any){
+        this.drinks.push
+    }
+
+
+    update(id:number ,updateDrinkDTO:any){
+
+        const existDrink = this.findOne(id)
+
+        if(existDrink){
+            const index = this.drinks.findIndex(drink => drink.id === id)
+
+            this.drinks[index]={
+                id,
+                ...updateDrinkDTO
+
+            }
+        }
+       
+    }
+
+
+    remove(id:number ){
+
+        const index = this.drinks.findIndex(drink => drink.id === id)
+
+        if(index >= 0){
+            this.drinks.splice(index,1)
+        }
+        
+        
+       
+    }
+
+
+
+
+
 }
