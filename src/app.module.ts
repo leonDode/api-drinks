72c3dde30022b90d9e0825a50978fdee7723329b
dode2511 <leondodejr@hotmail.com>
2024-06-30 22:53:08 -0300

    db conection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DrinksModule } from './drinks/drinks.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [DrinksModule],
+  imports: [DrinksModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
