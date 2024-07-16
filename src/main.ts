d98e661e6c0ec7c36d957e0a30b3876e05f07eb3
dode2511 <leondodejr@hotmail.com>
2024-07-15 22:53:56 -0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-    app.enableCors({
-       origin: 'https://api-drinks-git-main-leondodes-projects.vercel.app',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true,
-    });
+   
 
   const config = new DocumentBuilder()
     .setTitle('DrinkHub')
