7626b98e27869998c89cb9e6d4480d8e5495cfda
dode2511 <leondodejr@hotmail.com>
2024-07-15 23:17:41 -0300

    revert

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as express from 'express';
-import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Servir arquivos estáticos do Swagger
-  app.use('/public', express.static(join(__dirname, '..', 'public')));
+  
+    app.enableCors({
+       origin: 'https://api-drinks-git-main-leondodes-projects.vercel.app',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    });
 
   const config = new DocumentBuilder()
     .setTitle('DrinkHub')
@@ -22,16 +23,16 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document, {
     customSiteTitle: 'DrinkHub API Documentation',
-    customCssUrl: '/public/swagger-ui.css'
+    customCssUrl: '/api/swagger-ui.css',
   });
 
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true
-  }));
+    whitelist:true,
+    forbidNonWhitelisted:true,
+    transform:true 
+
+  }))
 
   await app.listen(3000);
 }
 bootstrap();
-
